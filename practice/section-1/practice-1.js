65bc405ab7c3d9e2ed56e74751b93bfad469e784
dingxin <zzuxind@163.com>
2019-08-21 10:48:29 +0800

    查找两数组中相同元素

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,14 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  var result=[];
+  for(let indexA=0;indexA<collectionA.length;indexA++){
+    for(let indexB=0;indexB<collectionB.length;indexB++){
+      if(collectionA[indexA] == collectionB[indexB]){
+        result.push(collectionA[indexA]);
+      }
+    }
+  }
+  return result;
+  // return '实现练习要求，并改写该行代码。';
 }
